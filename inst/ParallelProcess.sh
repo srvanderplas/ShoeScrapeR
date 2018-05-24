8bda065a3dfd7b0135707946a31098ebd35e8fd1
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-05-24 11:51:57 -0500

    Much better parallelization

--- inst/ParallelProcess.sh
+++ inst/ParallelProcess.sh
@@ -20,32 +20,48 @@ die() {
     exit 1
 }
 
-
-setup_folders() {
-  PNG_DIR="$1/pngs/"
-  TMP_DIR="$1/toslice/"
-  SLICE_DIR="$1/slices/"
+filter_images() {
+  usefulfile='useful_files.csv'
+  unusefulfile='not_useful_files.csv'
   
-  if [ ! -d "$1" ]; then
-    mkdir "$1"
-  fi
+  whiteThr=253
   
-  if [ ! -d "$PNG_DIR" ]; then
-    mkdir "$PNG_DIR"
-  fi
+  imgval=$(convert $1 -format "%[fx:mean*255]" info:)
+  imgvalint=$(printf %.0f $imgval)
+  # imgsize=$(identify -format "%[fx:w!=h]" $1 )
+  # imw=$(identify -format "%w" $1)
+  # imh=$(identify -format "%h" $1)
 
-  if [ ! -d "$TMP_DIR" ]; then
-    mkdir "$TMP_DIR"
-  fi
-  
-  if [ ! -d "$SLICE_DIR" ]; then
-    mkdir "$SLICE_DIR"
-  fi
+  toowhite=$(( $imgvalint > $whiteThr ))
+  if (( $toowhite == 1 )); then
+    echo "removing $1: mean value $imgval"
+  fi;
 
+  if (( $toowhite == 1 )); then
+    rm $1
+    echo $savestr >> $unusefulfile;
+  else 
+    echo $savestr >> $usefulfile;
+  fi;
 }
+export -f filter_images
 
 # Function to process a single shoe
 process_shoe() {
+  usage="$(basename "$0") [-h] [-e] [-m] [-x n] [-o n] [--overwrite] [--out <outdir>] file.jpg
+  
+  where:
+    -h            prints help
+    -e            uses canny edge detection on the image
+    -m            flips the image in x and y
+    -x n          sets the image chunk size to n x n
+    -o n          offsets the image by n x n pixels.
+    --overwrite   writes over previously generated intermediate images
+    --out         specifies the directory to store the intermediate and processed images. 
+                  Defaults to ./processed/. Directory will be created if it does not already exist."
+  
+  
+  #--- Read in arguments and set flag variables --------------------------------
   POSITIONAL=()
   
   # Initialize option variables
@@ -53,77 +69,48 @@ process_shoe() {
   EDGE=0
   OFFSET=0
   MIRROR=0
-  OUTPATH="./processed/"
+  OUTPATH="./processed"
   OVERWRITE=0
-  mirrorOpts="0 x y xy"
-
+  offset_re='^[0-9]+$'
+  option_re='^-'
+  
   # Assumes space separated, e.g. ./myscript.sh -e conf -s /etc -l /usr/lib /etc/hosts 
   while [[ $# -gt 0 ]]
     do
     key="$1"
     
     case $key in
+        -h)
+          echo "$usage"
+          exit
+          ;;
         -x|--size)
-          if [ "$2" ]; then
-            SIZE=$2
-            shift # past argument
-            shift # past value
-          else # no value provided
-            echo "Assuming defaultl size, 64 px"
-            shift # past argument
-          fi
+          SIZE=$2
+          shift # past argument
+          shift # past value
         ;;
         -e|--edge)
           EDGE=1
           shift # past argument
         ;;
         -o|--offset)
-          if [ "$2" ]; then 
+          if ![[ $2 =~ $offset_re ]]; then
+            OFFSET=32
+            shift # past argument
+          else 
             OFFSET="$2"
             shift # past argument
             shift # past value
-          else  # No value provided
-            echo "Assuming default offset, 32 px"
-            OFFSET=32
-            shift # past argument
           fi
         ;;
         -m|--mirror)
-          if [ "$2" ]; then
-            tmp="$2"
-            tmpInMirrorOpts= $(echo $mirrorOpts | xargs -n1 echo | grep -e \"^$tmp$\")
-            if [ -n "$tmpInMirrorOpts" ]; then 
-              die "-m should be one of $mirrorOpts. It was $tmp."
-            else 
-              case $tmp in 
-                0)
-                  MIRROR=0
-                ;;
-                x)
-                  MIRROR=1
-                ;;
-                y)
-                  MIRROR=2
-                ;;
-                xy)
-                  MIRROR=3
-                ;;
-              esac
-            fi
-          else 
-            echo "Assuming mirror on both x and y axis"
-            MIRROR=3
-          fi
+          MIRROR=1
+          shift
         ;;
         --out)
-          if [ "$2" ]; then
-            OUTPATH="$2"
-            shift # past argument
-            shift # past value
-          else 
-            echo "Writing to ./processed/. Directory will be created if necessary."
-            shift
-          fi
+          OUTPATH="$2"
+          shift # past argument
+          shift # past value
         ;;
         --overwrite)
           OVERWRITE=1
@@ -137,75 +124,131 @@ process_shoe() {
   done
   set -- "${POSITIONAL[@]}" # restore positional parameters
 
-  if [ "$#" > 1 ]; then 
+  if [ "$#" -gt 1 ]; then 
     echo "Too many left over arguments. Assuming $1 is the file path."
   fi
+  
+  # --- Set Script variables ---------------------------------------------------
   
   origfile=$(basename $1)
   basenoext=${origfile%.*}
   basefile="$basenoext.png"
   wkfile="$basenoext"
 
-  setup_folders $OUTPATH
+  # Ensure folders all exist
+  ORIG_DIR=$(dirname $1)
+  PNG_DIR="$OUTPATH/pngs"
+  TMP_DIR="$OUTPATH/toslice"
+  SLICE_DIR="$OUTPATH/slices"
   
-  PNG_DIR="$OUTPATH/pngs/"
-  TMP_DIR="$OUTPATH/toslice/"
+  if [ ! -d "$OUTPATH" ]; then
+    mkdir "$OUTPATH"
+  fi
+  
+  if [ ! -d "$PNG_DIR" ]; then
+    mkdir "$PNG_DIR"
+  fi
+
+  if [ ! -d "$TMP_DIR" ]; then
+    mkdir "$TMP_DIR"
+  fi
+  
+  if [ ! -d "$SLICE_DIR" ]; then
+    mkdir "$SLICE_DIR"
+  fi  
   
   # Ensure the simple operations are done
-  if [ "$OVERWRITE" ]; then
-    convert $ORIG_DIR$origfile $PNG_DIR$basefile
-    convert $PNG_DIR$basefile -trim $TMP_DIR$basefile
-  else 
-    # Convert to PNG
-    if [ ! -d $PNG_DIR$origfile ]; then
-      convert $ORIG_DIR$origfile $PNG_DIR$basefile
-    fi
+  # Convert to PNG
   
-    # Crop
-    if [ ! -d $CROP_DIR$basefile ]; then
-      convert $PNG_DIR$basefile -trim $TMP_DIR$basefile
-    fi
+  # echo $PNG_DIR/$basefile
+  # echo $ORIG_DIR/$origfile
   
+  if [ ! -f $PNG_DIR/$basefile ] || [ "$OVERWRITE" ]; then
+    convert $ORIG_DIR/$origfile $PNG_DIR/$basefile
   fi
-  
+
+  # Crop
+  if [ ! -f $CROP_DIR/$basefile ] || [ "$OVERWRITE" ]; then
+    convert $PNG_DIR/$basefile -trim $TMP_DIR/$basefile
+  fi
+
   wkfileprev=$wkfile
   if [ "$MIRROR" -eq "1" ]; then 
-    wkfile="$wkfileprev_flipX"
-    if [ ! -d "$TMP_DIR$wkfile.png" | "$OVERWRITE" ]; then
-      convert "$TMP_DIR$wkfileprev.png" -flop "$TMP_DIR$wkfile.png"
-    fi
-  else if [ "$MIRROR" -eq "2" ]; then
-    wkfile="$wkfileprev_flipY"
-    if [ ! -d "$TMP_DIR$wkfile.png" | "$OVERWRITE" ]; then
-      convert "$TMP_DIR$wkfileprev.png" -flip "$TMP_DIR$wkfile.png"
-    fi
-  else if [ "$MIRROR" -eq "3" ]; then
-    wkfile="$wkfileprev_flipXY"
-    if [ ! -d "$TMP_DIR$wkfile.png" | "$OVERWRITE" ]; then
-      convert "$TMP_DIR$wkfileprev.png" -flip -flop "$TMP_DIR$wkfile.png"
+    wkfile=$wkfileprev'_flip'
+    if [ ! -f $TMP_DIR/$wkfile.png ] || [ "$OVERWRITE" ]; then
+      convert $TMP_DIR/$wkfileprev.png -flip -flop $TMP_DIR/$wkfile'.png'
     fi
   fi
   
   wkfileprev=$wkfile
-  if [ "$EDGE" ]; then 
-    wkfile="$wkfile_edge"
-    if [ ! -d "$TMP_DIR$wkfile.png" | "$OVERWRITE" ]; then
-      convert "$TMP_DIR$wkfileprev.png" -canny 0x1+3%+3% -negate -colorspace Gray "$TMP_DIR$wkfile.png"
+  if [ "$EDGE" -eq "1" ]; then 
+    wkfile=$wkfile'_edge'
+    if [ ! -f $TMP_DIR/$wkfile.png ] || [ "$OVERWRITE" ]; then
+      convert $TMP_DIR/$wkfileprev.png -canny 0x1+3%+3% -negate -colorspace Gray $TMP_DIR/$wkfile'.png'
     fi
   fi
   
   wkfileprev=$wkfile
   if [ "$OFFSET" -gt "0" ]; then 
-    wkfile="$wkfile_offset$OFFSET"
-    if [ ! -d "$TMP_DIR$wkfile.png" | "$OVERWRITE" ]; then
-      convert "$TMP_DIR$wkfileprev.png" \
+    wkfile=$wkfile'_offset'$OFFSET
+    if [ ! -f $TMP_DIR/$wkfile.png ] || [ "$OVERWRITE" ]; then
+      convert $TMP_DIR/$wkfileprev.png \
         -gravity northeast \
         -background white \
-        -extent $(identify -format '%[fx:W+$OFFSET]x%[fx:H+$OFFSET]' "$TMP_DIR$wkfileprev.png" ) \
-        "$TMP_DIR$wkfile.png"
+        -extent $(identify -format '%[fx:W+$OFFSET]x%[fx:H+$OFFSET]' $TMP_DIR/$wkfileprev'.png' ) \
+        $TMP_DIR/$wkfile.png
     fi
   fi
   
+  # Prepare for cropping image
   
+  imw=$(identify -format "%w" $TMP_DIR/$wkfileprev.png)
+  imh=$(identify -format "%h" $TMP_DIR/$wkfileprev.png)
+  
+  full_tile_w=$(( $imw / $SIZE ))
+  full_tile_h=$(( $imh / $SIZE ))
+  
+  vcanv_w=$(( ($full_tile_w) * $SIZE ))  
+  vcanv_h=$(( ($full_tile_h) * $SIZE ))
+
+  cx=$(( ($vcanv_w - $imw ) / 2 ))
+  cy=$(( ($vcanv_h - $imh ) / 2 ))
+  
+  if [ "$cy" -ge "0" ]; then
+    cy='+'$cy
+  fi
+  if [ "$cx" -ge "0" ]; then
+    cx='+'$cx
+  fi
+  
+  vcanvszstr=$vcanv_w'x'$vcanv_h
+  
+  wkfileprev=$wkfile
+  wkfile=$wkfile'_crop'$vcanvszstr
+  
+  # echo "Full tiles: $full_tile_w x  $full_tile_h, cropping image to $vcanvszstr from $cx, $cy"
+  # echo "convert $TMP_DIR/$wkfileprev.png -repage $vcanvszstr$cx$cy -crop $vcanvszstr $TMP_DIR/$wkfile.png"
+  # convert $TMP_DIR/$wkfileprev.png -repage $vcanvszstr$cx$cy -crop $vcanvszstr $TMP_DIR/$wkfile.png
+  convert $TMP_DIR/$wkfileprev.png -repage $vcanvszstr+0+0 -crop $vcanvszstr $TMP_DIR/$wkfile.png
+  
+  
+  wkfileprev=$wkfile
+  wkfile=$wkfile'_sz'$SIZE
+  szstr=$SIZE'x'$SIZE
+      
+  convert $TMP_DIR/$wkfileprev.png -quiet -crop $szstr $SLICE_DIR/$wkfile'_%03d.png'
 
 }
+export -f process_shoe
+# 
+# process_shoe photos/adidas-originals-gazelle-tactile-yellow-black-gold_product_8894439_color_695418.jpg
+# process_shoe -e photos/adidas-originals-gazelle-tactile-yellow-black-gold_product_8894439_color_695418.jpg
+# process_shoe -m photos/adidas-originals-gazelle-tactile-yellow-black-gold_product_8894439_color_695418.jpg
+# process_shoe -e -m photos/adidas-originals-gazelle-tactile-yellow-black-gold_product_8894439_color_695418.jpg
+
+find ./photos -type f | parallel process_shoe -e {}
+find ./photos -type f | parallel process_shoe {}
+find ./photos -type f | parallel process_shoe -m -e {}
+find ./photos -type f |  parallel process_shoe -m {}
+
+find -path ./inst/processed/slices -type f  | parallel filter_images

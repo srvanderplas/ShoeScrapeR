12c7c4e1b5033725d5ae8e958aa4699b364b0c03
srvander <srvanderplas@gmail.com>
2019-02-01 07:59:12 -0600

    Don't use tidyverse - use separate packages, remove cruft

--- inst/GetAndProcessShoes.R
+++ inst/GetAndProcessShoes.R
@@ -1,7 +1,9 @@
 #!/usr/bin/Rscript
 
 library(ShoeScrapeR)
-library(tidyverse)
+library(purrr)
+library(magrittr)
+library(dplyr)
 library(stringr)
 library(RSelenium)
 
@@ -9,8 +11,7 @@ library(RSelenium)
 # try(system('docker run -d -p 4445:4444 -v /dev/shm:/dev/shm selenium/standalone-firefox'))
 try(system('docker run -p 4443:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:3.12.0'))
 # try(system('docker restart -p 4445:4444 -v /dev/shm:/dev/shm selenium/standalone-firefox'))
-# remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "firefox")
-# remDr$open()
+
 
 # Create a data frame of all combinations of parameters type and population
 fcn_opts <- expand.grid(type = c("new", "best", "rating"),
@@ -44,10 +45,6 @@ shoe_specific <- full_shoe_res %>%
     dl = purrr::map2_int(url, filename, ~try(download_image(.x, .y)))
   )
 
-
-# system("docker stop $(docker ps -a -q | grep chrome)")
-# system("docker rm $(docker ps -a -q)")
-
 try(system("rsync -avzu /home/srvander/Projects/CSAFE/ShoeScrapeR/extra/photos/ /home/srvander/Projects/CSAFE/LabelMe/Images/Shoes/"))
 
 try(system("rsync -avzu --no-perms --no-owner --no-group /home/srvander/Projects/CSAFE/ShoeScrapeR/extra/photos/ /myfiles/las/research/csafe/ShoeNeuralNet/ShoeImages/"))
@@ -63,65 +60,3 @@ write.table(data.frame(collection = "Shoes", file = flist), sep = ",",
             "/home/srvander/Projects/CSAFE/LabelMe/DirLists/labelme.txt", 
             row.names = F, col.names = F, quote = F)
 
-# Process image with bash script
-# system("../inst/ParallelProcess.sh")
-# chunkfiles <- list.files("processed/toslice/", full.names = F)
-# chunkfiles <- sort(chunkfiles)
-# chunk_df <- data_frame(
-#   image = str_replace(chunkfiles, ".png", ""),
-#   crop = str_extract(chunkfiles, "\\d{3,}x\\d{3,}"),
-#   flip = str_detect(chunkfiles, "_flip") %>% as.numeric(),
-#   edge = str_detect(chunkfiles, "_edge") %>% as.numeric()
-# ) %>%
-#   filter(!is.na(crop)) %>%
-#   mutate(image = str_replace(image, pattern = '(_flip)?(_edge)?_crop\\d{3,}x\\d{3,}', replacement = ''))
-# 
-# slicefiles <- list.files("processed/slices/", full.names = T) %>% sort()
-# slice_df <- data_frame(
-#   slice = basename(slicefiles),
-#   path = slicefiles,
-#   crop = str_extract(slice, "\\d{3,}x\\d{3,}"),
-#   flip = str_detect(slice, "_flip") %>% as.numeric(),
-#   edge = str_detect(slice, "_edge") %>% as.numeric(),
-#   size = str_extract(slice, "_sz\\d{2,}") %>% str_replace("_sz", "") %>% as.numeric(),
-#   image = str_replace(slice, "(_flip)?(_edge)?_crop\\d{3,}x\\d{3,}_sz\\d{2,}_\\d{3}.png", "")
-# ) %>%
-#   mutate(slice = str_extract(slice, "\\d{3}.png") %>% str_replace(".png", "") %>% as.numeric) %>%
-#   select(image, slice, path, crop, flip, edge, size)
-# 
-# 
-# # Write image list to database
-# con <- dbConnect(odbc::odbc(), "shoefeatures-connector")
-# if (exists("con")) {
-#   chunkfilesintable <- dbReadTable(con, "files")
-#   towrite <- anti_join(chunk_df, chunkfilesintable)
-#   dbWriteTable(con, "files", towrite, append = T)
-#   
-#   slicefilesintable <- dbReadTable(con, "slices")
-#   towrite <- anti_join(slice_df, slicefilesintable)
-#   dbWriteTable(con, "slices", towrite, append = T)
-#   
-#   # Ensure all files in database actually exist
-#   todropslices <- filter(slicefilesintable, !file.exists(path))
-#   todropfiles <- filter(chunkfilesintable, !file.exists(file.path("processed/toslice/", paste0(image, ".png"))))
-#   if (nrow(todropslices) > 0) {
-#     # Drop all rows in tmp table and overwrite
-#     dbWriteTable(con, "dropslices", todropslices, append = F, overwrite = T)
-#     
-#     qres <- dbSendStatement(con, "DELETE FROM slices WHERE path NOT IN (SELECT path FROM dropslices)")
-#     dbClearResult(qres)
-#   }
-#   
-#   if (nrow(todropfiles) > 0) {
-#     # Drop all rows in tmp table and overwrite
-#     dbWriteTable(con, "dropfiles", todropfiles, append = F, overwrite = T)
-#     
-#     qres <- dbSendStatement(con, "DELETE FROM files WHERE (path) NOT IN (SELECT path FROM dropfiles)")
-#     dbClearResult(qres)
-#   }
-#   
-#   dbDisconnect(con)
-# } else {
-#   stop("Couldn't connect to database")
-# }
-# 

7df396a35d79ba2361123719d26ee5d5f88c5618
srvander <srvanderplas@gmail.com>
2019-03-07 08:13:22 -0600

    switch to LSS
    Former-commit-id: c82247446dd4f1d5126f88601c1c674868ca8c6c

--- inst/GetAndProcessShoes.R
+++ inst/GetAndProcessShoes.R
@@ -46,13 +46,12 @@ shoe_specific <- full_shoe_res %>%
     dl = purrr::map2_int(url, filename, ~try(download_image(.x, .y)))
   )
 
-
 warning("Sync stage reached")
 
 system("rsync -avzu extra/photos/ ~/Projects/CSAFE/LabelMe/Images/Shoes/")
 
 # try(system("rsync -avzu --no-perms --no-owner --no-group extra/photos/ /myfiles/las/research/csafe/ShoeNeuralNet/ShoeImages/"))
-# try(system("rsync -avzu --no-perms --no-owner --no-group extra/photos/ /lss/research/csafe-shoeprints/ShoeNeuralNet/ShoeImages/"))
+try(system("rsync -avzu --no-perms --no-owner --no-group extra/photos/ /lss/research/csafe-shoeprints/ShoeNeuralNet/ShoeImages/"))
 
 system("find extra/photos/ -type f -name '*.jpg' > image_manifest")
 system("git add image_manifest inst/cron.log")
